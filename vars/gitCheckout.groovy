bfc9e8c985d7cca842fc94b77e4649f5bebecf4c
Dhanashreeraga <119791447+dhanashreeraga@users.noreply.github.com>
2025-04-09 16:15:16 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
  def call(String git_branch = 'main', String git_credentails = null ,String git_url  = null) {
    echo "this is a git checkout shared library"
   checkout([$class: 'GitSCM',
-/                       branches: [[name: "${git_branch}"]], 
+                       branches: [[name: "${git_branch}"]], 
                         userRemoteConfigs: [[credentialsId:"${git_credentails}",
                        url: "${git_url}"]]])
             sh 'pwd: ls -lrt'
